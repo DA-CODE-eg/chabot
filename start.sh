98258703db58feb52cd11126b53da92e16ee4e69
DA-CODE-eg <gruendgru@gmail.com>
2026-05-13 16:15:13 -0500

    admin en ruta /admin puerto 3000

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 node node_modules/puppeteer/install.mjs
+rm -rf /app/.wwebjs_auth/session-legal-segura/SingletonLock
+rm -rf /app/.wwebjs_auth/session-legal-segura/SingletonCookie
 node index.js
